d77ed0c51922dfbec28317601a8aaf859d2ea467
teeeem <cantwt22@gmail.com>
2016-04-18 20:56:53 -0400

    Partially working
    Can get opengl surface as DX Texture, working on getting input textures
    into the opengl scene.

---
+++ shadertoyRendererEGL/D3DAngleInterop'.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+using OpenTK.Platform;
+using OpenTK.Platform.Egl;
+
+namespace VVVV.DX11.Nodes
+{
+    public interface ID3DAngleInterop
+    {
+        void EnsureContext();
+        void MakeCurrent(IntPtr surface);
+        IntPtr CreateOffscreenSurface(int width, int height);
+        void DestroyOffscreenSurface(ref IntPtr surface);
+        IntPtr GetD3DSharedHandleForSurface(IntPtr egl_surface, int width, int height);
+        IntPtr CreateSharedSurfaceFromTexture(IntPtr d3d_texture, int width, int height);
+        void BindSurfaceToTexture(IntPtr surface);
+    }
+
+    public class D3DAngleInterop : ID3DAngleInterop, IDisposable
+    {
+        private readonly Control _control;
+        private readonly GraphicsContext _context;
+        private readonly IAngleWindowInfo _window_info;
+        //private readonly D3D9Interop _d3d9_interop;
+        //private readonly Dpi _dpi;
+        private bool _disposed;
+        //private Texture _texture;
+        //private IList<AngleImageSource> _sources = new List<AngleImageSource>();
+
+        public D3DAngleInterop()
+            : this(
+                new GraphicsMode(32, 24),
+                2, 0,
+                GraphicsContextFlags.Embedded
+                | GraphicsContextFlags.AngleD3D11
+                )
+        {
+        }
+
+        public D3DAngleInterop(GraphicsMode mode,
+            int major, int minor, GraphicsContextFlags flags)
+        {
+            //_d3d9_interop = new D3D9Interop();
+            //_dpi = new Dpi();
+
+            _control = new Control();
+            var win = OpenTK.Platform.Utilities.CreateWindowsWindowInfo(_control.Handle);
+            _window_info = OpenTK.Platform.Utilities.CreateAngleWindowInfo(win);
+            _context = new GraphicsContext(mode, _window_info, major, minor, flags);
+            _context.LoadAll();
+        }
+
+        //public IDpi Dpi
+        //{
+        //    get { return _dpi; }
+        //}
+
+        private IGraphicsContext Context
+        {
+            get { return _context; }
+        }
+
+        //public void AddUser(AngleImageSource source)
+        //{
+        //    _sources.Add(source);
+        //}
+
+        //public void RemoveUser(AngleImageSource source)
+        //{
+        //    _sources.Remove(source);
+        //}
+
+        ~D3DAngleInterop()
+        {
+            Dispose(true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose(bool called_from_finalizer)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            if (!called_from_finalizer)
+            {
+
+            }
+            // 
+            _disposed = true;
+        }
+
+        public void EnsureContext()
+        {
+            _context.MakeCurrent(_window_info);
+        }
+
+        public void MakeCurrent(IntPtr surface)
+        {
+            _window_info.MakeCurrent(surface);
+        }
+
+        public IntPtr GetD3DSharedHandleForSurface(IntPtr egl_surface, int width, int height)
+        {
+            var ptr = _window_info.QuerySurfacePointer(egl_surface);
+            //_texture = _d3d9_interop.CreateNewSharedTexture(ptr, width, height);
+            //return _texture.GetSurfaceLevel(0).NativePointer;
+            return ptr;
+        }
+
+        public IntPtr CreateSharedSurfaceFromTexture(IntPtr d3d_texture, int width, int height)
+        {
+            return _window_info.CreateSurfaceFromHandle(d3d_texture, width, height);
+        }
+
+        public IntPtr CreateOffscreenSurface(int width, int height)
+        {
+            return _window_info.CreateSurface(width, height);
+        }
+
+        public void DestroyOffscreenSurface(ref IntPtr surface)
+        {
+            _window_info.DestroySurface(ref surface);
+        }
+
+        public void SwapBuffers(IntPtr _surface)
+        {
+            _window_info.SwapBuffers(_surface);
+        }
+
+        public void BindSurfaceToTexture(IntPtr surface)
+        {
+            _window_info.BindSurfaceToTexture(surface);
+            
+        }
+    }
+}
--- shadertoyRendererEGL/shdertoyRenderer.Designer.cs
+++ shadertoyRendererEGL/shdertoyRenderer.Designer.cs
@@ -9,6 +9,9 @@ using System.Windows.Media.Imaging;
 using System.Drawing;
 using System.Reflection;
 
+using System.Collections;
+using System.Linq;
+
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -17,14 +20,15 @@ using VVVV.PluginInterfaces.V2;
 using VVVV.Utils.VColor;
 using VVVV.Core.Logging;
 
-using WpfGles;
+//using WpfGles;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.ES30;
+//using OpenTK.Graphics.ES20;
 using OpenTK.Platform;
 using OpenTK.Platform.Egl;
 
-using WpfGles.Interop;
+//using WpfGles.Interop;
 
 using FeralTic.DX11;
 using FeralTic.DX11.Resources;
@@ -34,29 +38,47 @@ using SlimDX.Direct3D11;
 namespace VVVV.DX11.Nodes
 {
     [PluginInfo(Author = "teem", Name = "Renderer", Category = "WPFEGL", Help = "WPF Widget housing an EGL Context", AutoEvaluate = true, InitialBoxHeight = 120, InitialBoxWidth = 160, InitialComponentMode = TComponentMode.InAWindow, InitialWindowHeight = 300, InitialWindowWidth = 400)]
-    public class shadertoyRendererNode : System.Windows.Forms.UserControl, IPluginEvaluate, IPartImportsSatisfiedNotification, IDX11ResourceProvider, IUserInputWindow
+    public class shadertoyRendererNode : System.Windows.Forms.UserControl, IPluginEvaluate, IPartImportsSatisfiedNotification, IDX11ResourceProvider, IUserInputWindow, IDisposable, IDX11ResourceDataRetriever
     {
         private readonly ElementHost _container = new ElementHost { Dock = System.Windows.Forms.DockStyle.Fill };
         private D3DAngleInterop interop;
 
         private IntPtr angleSurface;
         private int _program;
+        private int[] glTex = new int[4];
+        private int mPositionLoc;
+        private int mTexCoordLoc;
+        private DX11RenderContext shareContext;
+        Texture2DDescription desc;
 
         protected bool FInvalidate = false;
-        //private bool initTexture = true;
+
+        //private Texture2D tex = null;
+        private Texture2D t;
+        private SlimDX.DXGI.Resource SharedResource = null;
 
         private string _message = "Null";
 
         private int uColor;
 
+        private IntPtr dx11TexHandle;
         // ReSharper disable UnassignedField.Global
         // ReSharper disable MemberCanBePrivate.Global
+
+        [Import()]
+        protected IPluginHost FHost;
 
         [Input("Background Color", DefaultColor = new[] { 0.4, 0.4, 0.4, 1.0 }, IsSingle = true, HasAlpha = false, Order = 1)]
         public IDiffSpread<RGBAColor> ColorIn;
 
+        [Input("Texture In", IsSingle = true)]
+        protected Pin<DX11Resource<DX11Texture2D>> FTextureIn;
+
+        [Input("Texture Pointer", IsSingle = true, AsInt = true)]
+        protected ISpread<long> FTexturePointer;
+
         [Output("Texture")]
-        protected Pin<DX11Resource<DX11Texture2D>> FTextureOutput;
+        public Pin<DX11Resource<DX11Texture2D>> FTextureOutput;
 
         [Output("Height", AsInt = true, IsSingle = true, Order = 1)]
         public ISpread<int> HeightOut;
@@ -90,40 +112,9 @@ namespace VVVV.DX11.Nodes
         }
 
         void InitializeComponent()
-        {
-            try
-            {
-                FOutLog[0] = "Angle Interop was successful";
-
-  
-            }
-            catch
-            {
-                FOutLog[0] = "Angle Interop init failed";
-            }
-           
-            
+        {            
             Controls.Clear();
             var grid = new Grid();
-
-            //BitmapImage bmi = new BitmapImage();
-            //try
-            //{
-            //    bmi.BeginInit();
-            //    bmi.UriSource = new Uri("C:\\Users/user/Pictures/Saved Pictures/Sunrise-Over-Purple-Clouds-Wallpaper.jpg", UriKind.Absolute);
-            //    bmi.EndInit();
-                
-            //}
-            //catch
-            //{
-            //    //FOutLog[0] = "Couldn't Load Image";
-            //}
-            //var _image = new System.Windows.Controls.Image();
-            //_image.Source = bmi;
-
-            //_image.Stretch = System.Windows.Media.Stretch.Fill;
-            //_image.StretchDirection = StretchDirection.Both;
-            //grid.Children.Add(_image);
             _container.Child = grid;
 
             
@@ -133,7 +124,101 @@ namespace VVVV.DX11.Nodes
         // Called when data for any output pin is requested.
         public void Evaluate(int SpreadMax)
         {
-            return;
+            if (this.FTextureIn.PluginIO.IsConnected)
+            {
+
+                if (this.RenderRequest != null) { this.RenderRequest(this, this.FHost); }
+
+                if (this.AssignedContext == null) { this.SetNull(); return; }
+
+                this.FPointer.SliceCount = SpreadMax;
+
+                shareContext = this.AssignedContext;
+
+                if (this.FTextureIn[0].Contains(shareContext))
+                {
+                    if (t != null)
+                    {
+                        this.AssignedContext.CurrentDeviceContext.CopyResource(this.FTextureIn[0][shareContext].Resource, this.t);
+                        //_message = "Copying";
+                    }
+                }
+
+            }
+        
+
+                //    try
+                //    {
+                //        if (this.FTextureIn[0].Contains(context))
+                //        {
+                //            if (tex != null)
+                //            {
+                //                Texture2D t = this.FTextureIn[0][context].Resource;
+
+                //                if (t.Description.Width != this.tex.Description.Width
+                //                    || t.Description.Height != this.tex.Description.Height
+                //                    || t.Description.Format != this.tex.Description.Format)
+                //                {
+                //                    this.SharedResource.Dispose();
+                //                    this.SharedResource = null;
+                //                    this.tex.Dispose();
+                //                    this.tex = null;
+                //                }
+
+
+                //                if (t.Description.MipLevels > 1)
+                //                {
+                //                    this.FPointer[0] = 0;
+                //                    throw new Exception("Sharing texture with more than one mip level is not allowed");
+                //                }
+                //                if (t.Description.SampleDescription.Count > 1)
+                //                {
+                //                    this.FPointer[0] = 0;
+                //                    throw new Exception("Sharing multisampled texture is not allowed");
+                //                }
+
+                //            }
+                //            //Convert texture so it has no mips
+                //            if (tex == null)
+                //            {
+
+                //                Texture2D t = this.FTextureIn[0][context].Resource;
+                //                Texture2DDescription desc = t.Description;
+                //                desc.BindFlags = BindFlags.ShaderResource | BindFlags.RenderTarget;
+                //                desc.OptionFlags = ResourceOptionFlags.Shared;
+                //                desc.MipLevels = 1;
+                //                this.tex = new Texture2D(context.Device, desc);
+                //                this.SharedResource = new SlimDX.DXGI.Resource(this.tex);
+                //                this.FPointer[0] = SharedResource.SharedHandle.ToInt64();
+                //            }
+
+                //            this.AssignedContext.CurrentDeviceContext.CopyResource(this.FTextureIn[0][context].Resource, this.tex);
+                //        }
+                //        else
+                //        {
+                //            this.SetDefault(0);
+                //        }
+                //    }
+                //    catch
+                //    {
+                //        this.SetDefault(0);
+                //    }
+                //}
+                //else
+                //{
+                //    this.SetNull();
+                //}
+                return;
+        }
+
+        private void SetNull()
+        {
+            this.FPointer.SliceCount = 0;
+        }
+
+        private void SetDefault(int i)
+        {
+            this.FPointer[i] = 0;
         }
 
         public IntPtr InputWindowHandle
@@ -144,11 +229,10 @@ namespace VVVV.DX11.Nodes
         unsafe public void Update(IPluginIO pin, DX11RenderContext context)
         {
             FOutLog[0] = _message;
-            
-
+            //DX11RenderContext sharedContext = this.AssignedContext;
             try
             {
-                if (FInvalidate == true)
+                if (FInvalidate == true & this.FTextureIn.PluginIO.IsConnected)
                 {
                     var dummyTex = new DX11Resource<DX11Texture2D>();
 
@@ -157,28 +241,144 @@ namespace VVVV.DX11.Nodes
                     //D3DShareHandle =  interop.GetD3DSharedHandleForSurface(angleSurface, 500, 500);
                     // angleSurface = interop.CreateSharedSurfaceFromTexture(v4SharedHandle, 500, 500);
 
-                    IntPtr share = interop.GetD3DSharedHandleForSurface(angleSurface, 500, 500);
+                    IntPtr share = interop.GetD3DSharedHandleForSurface(angleSurface, 600, 600);
 
                     Texture2D tex = context.Device.OpenSharedResource<Texture2D>(share);
                     ShaderResourceView srv = new ShaderResourceView(context.Device, tex);
 
                     DX11Texture2D resource = DX11Texture2D.FromTextureAndSRV(context, tex, srv);
+                    SlimDX.DXGI.Resource angleRes = new SlimDX.DXGI.Resource(tex);
+                    
 
                     interop.EnsureContext();
+                    //Texture2D tex;
+                    SlimDX.DXGI.Resource sharedRes;
+                    IntPtr dxHandle = new IntPtr();
+                    IntPtr shared;
+
+                    
+
+                    try
+                    {
+
+
+                        //Texture2DDescription desc = tex.Description;
+                        //desc.Height = 256;
+                        //desc.Width = 256;
+
+                        Texture2D inTex = this.FTextureIn[0][context].Resource;
+                        if(inTex == null)
+                        {
+                            _message = "thiss isn't real";
+                        }
+                        Texture2DDescription desc, testDesc;
+                        try
+                        {
+                            testDesc = inTex.Description;
+                            desc = inTex.Description;
+                        }
+                        catch(Exception e)
+                        {
+                            testDesc = tex.Description;
+                            desc = tex.Description;
+                            _message = e.ToString();
+                        }
+
+                        // = inTex.Description;
+                        desc.Height = 256;
+                        desc.Width = 256;
+
+                        desc.BindFlags = BindFlags.ShaderResource | BindFlags.RenderTarget;
+                        desc.OptionFlags = ResourceOptionFlags.Shared;
+                        desc.MipLevels = 1;
+
+                        if(desc.MipLevels != testDesc.MipLevels)
+                        {
+                            _message = "MIPS";
+                        }
+
+                        if (desc.ArraySize != testDesc.ArraySize)
+                        {
+                            _message = "Array";
+                        }
+
+                        if (desc.CpuAccessFlags != testDesc.CpuAccessFlags)
+                        {
+                            _message = "CPU";
+                        }
+
+                        if (desc.SampleDescription != testDesc.SampleDescription)
+                        {
+                            _message = "SAMPLE";
+                        }
+
+                        if (desc.Usage != testDesc.Usage)
+                        {
+                            _message = "Usage";
+                        }
+
+
+                        t = new Texture2D(context.Device, desc);
+
+                        sharedRes = new SlimDX.DXGI.Resource(t);
+                        dxHandle = sharedRes.SharedHandle;
+                        //_message = FTexturePointer[0].ToString();
+                        ////FTexturePointer
+
+                        ////int p = unchecked((int)this.FTexturePointer[0]);
+                        ////IntPtr shared = new IntPtr(FTexturePointer[0]);
+
+                        //if (shared != null)
+                        //{
+                        shared =  interop.CreateSharedSurfaceFromTexture(dxHandle, desc.Width, desc.Height);
+                        //shared = interop.CreateSharedSurfaceFromTexture(share, 600, 600);
+                        //}
+                        FTextureOutput[0] = new DX11Resource<DX11Texture2D>();
+                        //FTextureOutput[0][context] = t;
+
+
+                        ShaderResourceView tSRV = new ShaderResourceView(context.Device, t);
+                        DX11Texture2D tRes = DX11Texture2D.FromTextureAndSRV(context, t, tSRV);
+                        FTextureOutput[0][context] = tRes;
+
+
+                        GL.GenTextures(1, glTex);
+                        GL.BindTexture(TextureTarget.Texture2D, glTex[0]);
+
+                        interop.BindSurfaceToTexture(shared);
+
+                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) TextureMinFilter.Nearest);
+                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int) TextureMinFilter.Nearest);
+
+                        //GL.ActiveTexture(TextureUnit.Texture0);
+                        //GL.BindTexture();
+
+                    }
+                    catch (Exception e)
+                    {
+                        //_message = e.ToString();
+                    }
 
                     var vert = @"//#version 300 es
                         attribute vec4 vPosition;
+                        attribute vec2 aTexCoord;
+                        varying vec2 vTexCoord;
                         void main()
                         {
                             gl_Position = vPosition;
+                            vTexCoord = aTexCoord;
                         }";
 
                     var frag = @"//#version 300 es
                         precision mediump float;
+                        varying vec2 vTexCoord;
                         uniform vec4 color;
+                        uniform sampler2D iChannel0;
                         void main()
                         {
-                            gl_FragColor = color;
+                            vec2 uv = vTexCoord;
+                            vec3 col = texture2D(iChannel0, uv).xyz;
+                            gl_FragColor = vec4(col, 1.0);
                         }";
 
 
@@ -231,40 +431,84 @@ namespace VVVV.DX11.Nodes
                     }
 
                     uColor = GL.GetUniformLocation(_program, "color");
+                    mPositionLoc = GL.GetAttribLocation(_program, "vPosition");
+                    mTexCoordLoc = GL.GetAttribLocation(_program, "aTexCoord");
 
                     FPointer[0] = share.ToInt64();
                     FValid[0] = true;
                     FInvalidate = false;
+                    
+                    //int[] glTex = new int[4];
+                    //GL.GenTextures(1, glTex);
+                    //GL.TexParameter(GL)
+                    //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) TextureMagFilter.Nearest);
                 }
+
+                //this.AssignedContext.CurrentDeviceContext.CopyResource(this.FTextureIn[0][context].Resource, t);
+                context.CurrentDeviceContext.CopyResource(FTextureIn[0][context].Resource, t);
 
                 interop.MakeCurrent(angleSurface);
 
                 float[] vVertices = new float[] {0.0f, 0.5f, 0.0f,
                  -0.5f, -0.5f, 0.0f,
                  0.5f, -0.5f, 0.0f};
+                
+
+                float[] vertices = new float[]
+                {
+                    -0.5f,  0.5f, 0.0f,  // Position 0
+                     0.0f,  0.0f,        // TexCoord 0
+                    -0.5f, -0.5f, 0.0f,  // Position 1
+                     0.0f,  1.0f,        // TexCoord 1
+                     0.5f, -0.5f, 0.0f,  // Position 2
+                     1.0f,  1.0f,        // TexCoord 2
+                     0.5f,  0.5f, 0.0f,  // Position 3
+                     1.0f,  0.0f         // TexCoord 3
+                };
+
+                short[] indices = { 0, 1, 2, 0, 2, 3 };
+
                 GL.Viewport(0, 0, 500, 500);
-                GL.ClearColor(0.2f, 0.7f, 1.0f, 1.0f);
+                GL.ClearColor((float)ColorIn[0].R, (float)ColorIn[0].G, (float)ColorIn[0].B, 0.0f);
                 GL.Clear(ClearBufferMask.ColorBufferBit);
                 GL.UseProgram(_program);
                 Color4 color = new Color4( (float)ColorIn[0].R, (float)ColorIn[0].G, (float)ColorIn[0].B, (float)ColorIn[0].A);
                 GL.Uniform4(uColor, color);
 
-                GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, vVertices);
-                GL.EnableVertexAttribArray(0);
+                //Vertex Attributes
+                GL.VertexAttribPointer(mPositionLoc, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), vertices);
+                // Load the texture coordinate
+                GL.VertexAttribPointer(mTexCoordLoc, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), vertices.Skip(3).ToArray());
+
+
+                //GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, vVertices);
+                //GL.EnableVertexAttribArray(0);
+                GL.EnableVertexAttribArray(mPositionLoc);
+                GL.EnableVertexAttribArray(mTexCoordLoc);
+
+                //Texture Bind
+                GL.ActiveTexture(TextureUnit.Texture0);
+                GL.BindTexture(TextureTarget.Texture2D, glTex[0]);
+                
+
+                int iChannel0 = GL.GetUniformLocation(_program, "iChannel0");
+                GL.Uniform1(iChannel0, 0);
+
+
                 GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+                GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedShort, indices);
 
                 interop.SwapBuffers(angleSurface);
                 GL.Finish();
-                _message = "running update";
+                //_message = "running update";
 
             }
             catch (Exception e)
             {
 
                 //_message = e.ToString();
+                //_message = e.ToString();
             }
-
-            //FOutLog[0] = "Got caught up";
 
         }
 
@@ -273,5 +517,13 @@ namespace VVVV.DX11.Nodes
             for (int i = 0; i < FTextureOutput.SliceCount; i++)
                 this.FTextureOutput[i].Dispose(context);
         }
+
+        public DX11RenderContext AssignedContext
+        {
+            get;
+            set;
+        }
+
+        public event DX11RenderRequestDelegate RenderRequest;
     }
 }
